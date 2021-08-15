0cbeda7e831e35201c358e97c3e654b68925ee46
jinnuT <jinnuaz@hotmail.com>
2021-08-15 12:28:39 -0700

    Terraform and node-exporter

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -30,7 +30,7 @@ resource "aws_ec2_tag" "name-tag-app" {
   resource_id                     = element(aws_spot_instance_request.app-instances.*.spot_instance_id,count.index)
   key                             = "Name"
   value                           = "${element(var.APP_COMPONENTS,count.index)}-${var.ENV}"
-  Monitor                         = "yes"
+  monitoring                      = true
 }
 
 resource "aws_ec2_tag" "name-tag-db" {
